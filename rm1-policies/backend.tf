3c40198f2e8116d7348de3532b9a5f3e0a8187d9
burhanuddin-anw <burhanuddin.sk@appnetwise.com>
2026-04-21 19:54:53 +0530

    added stg cintainer key

--- rm1-policies/backend.tf
+++ rm1-policies/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "rg-rm1-terraform-state"
     storage_account_name  = "strm1terraformstate"
     container_name        = "terraform-state"
-    key                   = "terraform.tfstate"
+    key                   = "dev.tfstate"
   }
 }
